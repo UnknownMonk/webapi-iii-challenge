4ed469eca4d0fb19df2756a2a2900003e1c8a646
UnknownMonk <smodcartoon@gmail.com>
2019-03-20 16:25:39 -0400

    testing deployment

--- server.js
+++ server.js
@@ -9,8 +9,5 @@ server.use(express.json());
 
 server.use('/api/posts', posts);
 server.use('/api/user', user);
-server.use('/', (req, res) => {
-  res.status(200).json('Heroku is hiting the home route on my express app');
-});
 
 module.exports = server;
