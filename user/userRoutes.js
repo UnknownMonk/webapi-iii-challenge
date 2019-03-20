ba30a7ad762b3d7ef56bf32c48eca8d995e9cb2e
UnknownMonk <smodcartoon@gmail.com>
2019-03-20 14:41:41 -0400

    Used getUserPosts() in a get route

--- user/userRoutes.js
+++ user/userRoutes.js
@@ -99,4 +99,23 @@ router.put('/:id', (req, res) => {
   }
 });
 
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  db.getUserPosts(id)
+    .then(post => {
+      if (post.length > 0) {
+        res.status(200).json(post);
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist.' });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: 'The post information could not be retrieved.' });
+    });
+});
+
 module.exports = router;
